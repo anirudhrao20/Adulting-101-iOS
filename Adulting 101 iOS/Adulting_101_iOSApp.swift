77ec4b508143b6c8206230110aa6d1dec0398eba
Anirudh Rao <contactme@anirudhrao.dev>
2024-02-18 20:08:03 -0800

    Initial Commit

---
+++ Adulting 101 iOS/Adulting_101_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adulting_101_iOSApp.swift
+//  Adulting 101 iOS
+//
+//  Created by Anirudh Rao on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Adulting_101_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
